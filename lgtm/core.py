9bc16c5f83b0f9d6b480f3f30cfc4ef4707856c4
Ryota Saito <saito@RYOTAnoMacBook-Air.local>
2020-06-14 22:14:03 +0900

    complete

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,4 +1,6 @@
 import click
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
 
 @click.command()
 @click.option('--message', '-m', default='LGTM', show_default=True, help='画像に載せる文字列')
@@ -9,4 +11,5 @@ def cli(keyword, message):
     click.echo('lgtm') 
 
 def lgtm(keyword, message):
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
